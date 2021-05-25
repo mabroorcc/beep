4ab1c6cd0864341ae9a3ace7821c7001e12f47e9
mabroor007 <amabroor007@gmail.com>
2021-05-26 01:57:15 +0500

    change promiseduploadcall

--- client-service/src/features/FileUpload/promisedCall.ts
+++ client-service/src/features/FileUpload/promisedCall.ts
@@ -1,11 +1,12 @@
 import { store } from "../../app/store";
 import { uploadFileAction } from "./uploadSlice";
 
+// this call is through socket connection
 export const PromisedFileUpload = (
   filename: string,
   file: Blob
 ): Promise<string> => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     store.dispatch(
       uploadFileAction(filename, file, (url) => {
         resolve(url);
